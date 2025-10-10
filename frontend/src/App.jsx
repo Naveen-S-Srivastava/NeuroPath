f6b843b3dbd1655bf70ed4eefaacf5b32e8e2032
yashraj-03 <yashraj703729@gmail.com>
2025-10-10 21:29:18 +0530

    Dark/light mode and main file setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import React from 'react';
+import { AuthProvider } from './components/context/AuthContext';
+import { AppRouter } from './components/routing/AppRouter';
+import { Toaster } from './components/ui/sonner';
 
-function App() {
-
+const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <AuthProvider>
+      <AppRouter />
+      <Toaster position="top-right" />
+    </AuthProvider>
+  );
+};
 
-export default App
+export default App;
